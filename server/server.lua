5b75f7b1a123450e01c0d5271672108c1cd38561
Puntzi <70822492+Puntzi@users.noreply.github.com>
2022-09-27 14:21:00 +0200

    can use ESX inventory

--- server/server.lua
+++ server/server.lua
@@ -1,12 +1,21 @@
-local ox_inventory = exports.ox_inventory
+if Config.UseOxInventory then
+    local ox_inventory = exports.ox_inventory
 
-
-RegisterNetEvent('pnt_farming:giveItem', function(item)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    print(item)
-    if ox_inventory:CanCarryItem(source, item, 1) then
-        ox_inventory:AddItem(source, item, 1, nil, nil)
-    else
-        xPlayer.showNotification(_U('cant_carry'))
-    end
-end)
+    RegisterNetEvent('pnt_farming:giveItem', function(item)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        if ox_inventory:CanCarryItem(source, item, 1) then
+            ox_inventory:AddItem(source, item, 1, nil, nil)
+        else
+            xPlayer.showNotification(_U('cant_carry'))
+        end
+    end)
+else
+    RegisterNetEvent('pnt_farming:giveItem', function(item)
+        local xPlayer = ESX.GetPlayerFromId(source)
+        if xPlayer.canCarryItem(item, 1) then 
+            xPlayer.addInventoryItem(item)
+        else 
+            xPlayer.showNotification(_U('cant_carry'))
+        end
+    end)
+end
