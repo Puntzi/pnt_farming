69eadcc48bf967d7ed88134f7946ff6e7213376d
Puntzi <70822492+Puntzi@users.noreply.github.com>
2022-09-29 11:17:52 +0200

    1.1.0

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,12 @@
 Locales['en'] = {
     ['pickup_fruit'] = '[E] - Pickup %s',
+    ['interact_process_fruit'] = '[E] - Process Fruits',
+    ['sell_box'] = '[E] - Sell box of fruits',
     ['canceled'] = 'You canceled the acction',
     ['cant_carry'] = "Can't carry more items",
-    ['map_blip'] = '%s farm'
+    ['map_blip'] = '%s farm',
+    ['process_zone'] = 'Process fruit',
+    ['sell_zone'] = 'Sell fruit',
+    ['not_enough_fruits'] = 'Not enough fruits to process need at least %s',
+    ['any_box_to_sell'] = 'You dont have any box to sell'
 }
